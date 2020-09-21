d80cf123fce5581d97b3fd27f60a6e9df138d9e6
jiaengoh <hihihi_97@hotmail.com>
2020-09-21 20:50:51 +0800

    a
    a

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 $( ".inner-switch" ).on("click", function() {
-    if( $( "body" ).hasClass( "dark" )) {
-      $( "body" ).removeClass( "dark" );
-      $( ".inner-switch" ).text( "OFF" );
-    } else {
-      $( "body" ).addClass( "dark" );
-      $( ".inner-switch" ).text( "ON" );
-    }
+  if( $( "body" ).hasClass( "dark" )) {
+    $( "body" ).removeClass( "dark" );
+    $( ".inner-switch" ).text( "OFF" );
+  } else {
+    $( "body" ).addClass( "dark" );
+    $( ".inner-switch" ).text( "ON" );
+  }
 });
 
-
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
+// function myFunction() {
+//   var element = document.body;
+//   element.classList.toggle("dark-mode");
+// }
 
 
